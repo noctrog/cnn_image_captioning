db0c1a16536af63fd655e849fa5520ddacdcd6a7
noctrog <ramon1999cg@gmail.com>
2019-11-01 19:50:47 +0100

    NLLoss

--- train_cnn_cnn.py
+++ train_cnn_cnn.py
@@ -6,11 +6,10 @@ import numpy as np
 
 import torch
 from torch import optim
+from torch.utils.tensorboard import SummaryWriter
 from torchvision import datasets, transforms
 
 import model
-
-from tensorboardX import SummaryWriter
 
 # Usar CUDA si esta disponible
 device = torch.device('cuda' if torch.cuda.is_available else 'cpu')
@@ -82,18 +81,18 @@ def main(args):
     cnn_cnn = model.CNN_CNN_HA().to(device)
 
     # algoritmo que actualizara los pesos de las redes
-    optimizer = optim.Adam(cnn_cnn.prediction_module.parameters(), lr=args.lr)
+    optimizer = optim.Adam(cnn_cnn.parameters(), lr=args.lr)
 
     # Funcion de perdida a usar: Entropia cruzada ya que los elementos a predecir (palabras)
     # son mutuamente exlusivos (solo se puede elegir una palabra)
-    criterion = torch.nn.CrossEntropyLoss()
+    criterion = torch.nn.NLLoss()
 
+    losses = []
+    mean_losses = []
     for e in range(args.epochs):
         # Crear un generador
         trainloader = dataloader(args.image_folder, args.captions_file, args.batch_size)
 
-        losses = []
-        mean_losses = []
         for batch, (images, captions) in enumerate(trainloader):
             # Pasa las imanes a la tarjeta grafica en caso de que haya una
             images_v = images.to(device)
@@ -105,8 +104,7 @@ def main(args):
             # Inicializa el tensor de embeddings
             train_embeddings = torch.zeros(args.batch_size, max_length+1, cnn_cnn.embedding.dim)
             # Inserta el embedding de empezar la frase (todo a 1)
-            train_embeddings[:][0] = 1.0
-            # train_embeddings[:][0][:] = torch.stack([first_word for _ in range(args.batch_size)], dim=0)
+            train_embeddings[:, 0, :] = 1.0
             # Inserta los embeddings de las palabras de cada frase
             for i, indices in enumerate(train_indices):
                 train_embeddings[i][1:len(indices)+1] = cnn_cnn.embedding.vectors[indices]
@@ -119,6 +117,7 @@ def main(args):
             for i, label in enumerate(train_labels):
                 valid_training_indices = valid_training_indices + [j for j in range(i*(max_length+1), i*(max_length+1) + len(label)+1)]
 
+
             # Genera el vector de salidas esperadas por el entrenamiento
             expected_ids = [[cnn_cnn.stoi[word] if word in cnn_cnn.stoi else cnn_cnn.stoi['<unknown>'] for word in label] for label in captions]
             # Anadir como palabra final el fin de clase
@@ -130,27 +129,19 @@ def main(args):
             flat_expected_ids = [i for ids in expected_ids for i in ids]
             expected_v = torch.LongTensor(flat_expected_ids).to(device)
 
-            # Genera las salidas esperadas en forma de one-hot
-            # expected_labels = [label[1:] for label in captions]
-            # Se usa tambien la palabra menos usada como salida en caso de que la palabra no este en el
-            # diccionario (porque la entrada es embedding y la salida es one-hot, da igual)
-            # expected_indices = [[cnn_cnn.stoi[word] if (word in cnn_cnn.stoi) else 399999 for word in label] for label in expected_labels]
-            # Anadir el fin de linea para que el modelo pueda predecirlo (posicion 400000, no esta en el diccionario porque no hace falta)
-            # expected_indices = [label + [400000] for label in expected_indices]
-            # Pasar resultados esperados a un tensor one-hot
-            # max_length = max([len(label) for label in expected_indices])
-            # expected_v = torch.zeros((args.batch_size, max_length, cnn_cnn.vocab_size))
-            # for i, indices in enumerate(expected_indices):
-                # indices_v = torch.tensor(indices)
-                # expected_v[i][:len(indices)] = torch.nn.functional.one_hot(indices_v, cnn_cnn.vocab_size)
-            # expected_v = expected_v.to(device).long()
-
             # Limpia los optimizadores
             optimizer.zero_grad()
 
             # Calcula las predicciones
             outputs_v = cnn_cnn(images_v, captions_v)
 
+            print(train_labels)
+            print(expected_ids)
+            print(expected_v)
+            # _, max_ids = torch.max(outputs_v, dim=2)
+            # max_ids, _ = torch.max(max_ids, dim=1)
+            print(captions_v)
+            input()
             # Desenrolla las frases generadas para poder pasarlas por la funcion de perdida
             outputs_v = outputs_v.view(-1, cnn_cnn.vocab_size)
 
@@ -194,7 +185,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--lr", type=float, default=0.002, help='Learning rate')
     parser.add_argument("--epochs", type=int, default=1, help="Numero de generaciones a entrenar")
-    parser.add_argument("--batch_size", type=int, default=1, help="Tamaño del batch")
+    parser.add_argument("--batch_size", type=int, default=8, help="Tamaño del batch")
     parser.add_argument("--image_folder", type=str, help='Carpeta que contiene todas las imagenes')
     parser.add_argument("--val_image_folder", type=str, help='Carpeta que contiene todas las imagenes para validar el modelo')
     parser.add_argument("--captions_file", type=str, help='Archivo JSON que contiene las frases')
