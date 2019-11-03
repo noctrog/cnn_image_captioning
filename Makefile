430b87c58e4047a2a3d1196cea4b8b8f9991bb08
noctrog <ramon1999cg@gmail.com>
2019-11-03 16:30:36 +0100

    Generar vocabulario para entrenar el embedding propio

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
-BATCH_SIZE=1
-LEARNING_RATE=1e-4
+BATCH_SIZE=10
+LEARNING_RATE=1e-5
+EPOCHS=40
 
 TRAIN_IMAGE_FOLDER=~/Documents/Machine\ Learning/datasets/COCO/train2017
 TRAIN_ANNOTATION_FILE=~/Documents/Machine\ Learning/datasets/COCO/annotations/captions_train2017.json
@@ -10,7 +11,13 @@ TEST_ANNOTATION_FILE=~/Documents/Machine\ Learning/datasets/COCO/annotations/cap
 train_cnn_cnn:
 	python train_cnn_cnn.py --batch_size 16 --image_folder $(TRAIN_IMAGE_FOLDER) --captions_file $(TRAIN_ANNOTATION_FILE) \
 				--val_image_folder $(TEST_IMAGE_FOLDER) --val_captions_file $(TEST_ANNOTATION_FILE) \
-				--batch_size $(BATCH_SIZE) --lr $(LEARNING_RATE)
+				--batch_size $(BATCH_SIZE) --lr $(LEARNING_RATE) --epochs $(EPOCHS)
 
 generate_mini_glove:
 	python recortar_glove.py --file $(TRAIN_ANNOTATION_FILE)
+
+inference:
+	python inference.py --images $(TRAIN_IMAGE_FOLDER) --captions $(TRAIN_ANNOTATION_FILE)
+
+dicts:
+	python gen_dicts.py --file $(TRAIN_ANNOTATION_FILE)
---
+++ gen_dicts.py
@@ -0,0 +1,47 @@
+import argparse
+import json
+import re
+
+import torch
+from spellchecker import SpellChecker
+
+def main(args):
+
+    spell = SpellChecker(distance=1)
+
+    print('Leyendo palabras...')
+    word_set = set()
+    with open(args.file, "r") as captions_file:
+        data = json.load(captions_file)
+        for i in range(len(data['annotations'])):
+            words = re.findall(r"[\w']+|[.,!?;]", data['annotations'][i]['caption'])
+            for word in words:
+                word_set.add(word)
+
+    print('Corrigiendo faltas...')
+    correct_word_set = set([spell.correction(word).lower() for word in word_set])
+    print(len(word_set))
+    print(len(correct_word_set))
+    word_list = list(correct_word_set)
+
+    print('Guardando diccionarios...')
+    ids = [i for i in range(len(word_list))]
+    # Generar stoi
+    stoi = dict(zip(word_list, ids))
+    # Generar itos
+    itos = dict(zip(ids, word_list))
+
+    # Guardar diccionarios
+    dicts = {'stoi': stoi,
+             'itos': itos}
+    torch.save(dicts, './weights/dicts')
+    print('Hecho.')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file", type=str, default='', help='JSON que contiene los captions')
+    args = parser.parse_args()
+
+    assert args.file != ''
+
+    main(args)
